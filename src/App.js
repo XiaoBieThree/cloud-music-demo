5ed80351c95494abeb99f036e9b31f8ac28bd688
TongMutian001 <1125145878@qq.com>
2021-01-19 17:59:38 +0800

    create router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { GlobalStyle } from './style';  // 全局样式
 import { IconStyle } from './assets/iconfont/iconfont'; // 字体
+import { renderRoutes } from 'react-router-config'; // renderRoutes 读取路由配置转化为 Route 标签
+import routes from './routes/index';
+import { HashRouter } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle />
-      <IconStyle />
-      <i className="iconfont">&#xe62b;</i>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {/* <i className="iconfont">&#xe62b;</i> */}
+        { renderRoutes (routes) }
+      </HashRouter>
     </div>
   );
 }
---
+++ src/routes/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Redirect } from "react-router-dom";
+import Home from '../application/Home';
+import Recommend from '../application/Recommend/index';
+import Singers from '../application/Singers/index';
+import Rank from '../application/Rank/index';
+
+export default [
+  {
+    path: "/",
+    component: Home,
+    routes: [
+      {
+        path: "/",
+        exact: true,
+        render: () => (
+          <Redirect to={"/home"}/>
+        )
+      },
+      {
+        path: "/recommend",
+        component: Recommend
+      },
+      {
+        path: "/singers",
+        component: Singers
+      },
+      {
+        path: "/rank",
+        component: Rank
+      }
+    ]
+  }
+]
