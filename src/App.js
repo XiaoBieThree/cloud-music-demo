d3a264bb12210da295d6976511ee3837d42945cc
TongMutian001 <1125145878@qq.com>
2021-01-20 17:24:07 +0800

    pour into redux(store and reducer)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store/index';
 import { GlobalStyle } from './style';  // 全局样式
 import { IconStyle } from './assets/iconfont/iconfont'; // 字体
 import { renderRoutes } from 'react-router-config'; // renderRoutes 读取路由配置转化为 Route 标签
 import routes from './routes/index';
 import { HashRouter } from 'react-router-dom';
 
-
 function App() {
   return (
-    <div className="App">
+    <div className="App" store={store}>
       <HashRouter>
         <GlobalStyle></GlobalStyle>
         <IconStyle></IconStyle>
--- src/application/Home/index.js
+++ src/application/Home/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { renderRoutes } from "react-router-config";
+import { Top, Tab, TabItem } from './style';
+import { NavLink } from 'react-router-dom';
 
 function Home (props) {
   const { route } = props;
   console.log(props);
   return (
     <div>
-      <div>Home</div>
+      <Top>
+        <span className="iconfont menu">&#xe65c;</span>
+        <span className="title">WebAppDemo</span>
+        <span className="iconfont search">&#xe62b;</span>
+      </Top>
+      <Tab>
+        <NavLink to="/recommend" activeClassName="selected"><TabItem><span > 推荐 </span></TabItem></NavLink>
+        <NavLink to="/singers" activeClassName="selected"><TabItem><span > 歌手 </span></TabItem></NavLink>
+        <NavLink to="/rank" activeClassName="selected"><TabItem><span > 排行榜 </span></TabItem></NavLink>
+      </Tab>
       { renderRoutes (route.routes) }
     </div>
   )
