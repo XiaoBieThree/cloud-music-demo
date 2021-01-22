cd24632ee673fea778eacaf0360c59f72f5d9e4d
TongMutian001 <1125145878@qq.com>
2021-01-22 17:57:43 +0800

    change hashRouter as BrowserRouter

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import { GlobalStyle } from './style';  // 全局样式
 import { IconStyle } from './assets/iconfont/iconfont'; // 字体
 import { renderRoutes } from 'react-router-config'; // renderRoutes 读取路由配置转化为 Route 标签
 import routes from './routes/index';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App" store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <GlobalStyle></GlobalStyle>
         <IconStyle></IconStyle>
         {/* <i className="iconfont">&#xe62b;</i> */}
         { renderRoutes (routes) }
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -10,15 +10,15 @@ function Slider (props) {
 
   useEffect (() => {
     if (bannerList.length && !sliderSwiper){
-        let newSliderSwiper = new Swiper(".slider-container", {
-          loop: true,
-          autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-          },
-          pagination: {el:'.swiper-pagination'},
-        });
-        setSliderSwiper(newSliderSwiper);
+      let newSliderSwiper = new Swiper(".slider-container", {
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        pagination: {el:'.swiper-pagination'},
+      });
+      setSliderSwiper(newSliderSwiper);
     }
   }, [bannerList.length, sliderSwiper]);
   
@@ -27,15 +27,13 @@ function Slider (props) {
       <div className="slider-container">
         <div className="swiper-wrapper">
           {
-            bannerList.map (slider => {
-              return (
-                <div className="swiper-slide" key={slider.imageUrl}>
-                  <div className="slider-nav">
-                    <img src={slider.imageUrl} width="100%" height="100%" alt="推荐" />
-                  </div>
+            bannerList.map (slider => (
+              <div className="swiper-slide" key={slider.imageUrl}>
+                <div className="slider-nav">
+                  <img src={slider.imageUrl} width="100%" height="100%" alt="推荐" />
                 </div>
-              );
-            })
+              </div>
+            ))
           }
         </div>
         <div className="swiper-pagination"></div>
